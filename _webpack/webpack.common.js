ff383e7c5b7a696323e70e10a2d8ca67dab0d9c7
Bharath Kumar <avibharath.s@gmail.com>
2020-01-16 22:14:07 +0100

    feat: files

--- _webpack/webpack.common.js
+++ _webpack/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -7,6 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../_dist'),
     filename: '[name].bundle.js',
+    //publicPath: '/',
   },
   module: {
     rules: [
@@ -19,4 +21,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My Website',
+      template: path.resolve(__dirname, '../_templates/client.html'),
+    }),
+  ],
 };
